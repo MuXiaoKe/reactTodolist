454e401a88352a2fa028b925bb6ea9af5789d9d4
MuXiaoKe <745633560@qq.com>
2018-11-21 16:26:38 +0800

    update account

--- src/cpts/accountbook.js
+++ src/cpts/accountbook.js
@@ -1,25 +1,107 @@
 import React, { Component } from "react";
-import { Form, Input, Button } from "antd";
+import dayjs from "dayjs";
+import { Form, Input, Button, Table, Tag } from "antd";
 
 const FormItem = Form.Item;
+
+const columns = [
+	{
+		title: "金额",
+		dataIndex: "money",
+		key: "money",
+		render: text => <a href="javascript:;">{text}</a>
+	},
+	{
+		title: "时间",
+		dataIndex: "time",
+		key: "time"
+	},
+	{
+		title: "类型",
+		key: "tags",
+		dataIndex: "tags",
+		render: tags => (
+			<span>
+				{tags.map(tag => (
+					<Tag color="blue" key={tag}>
+						{tag}
+					</Tag>
+				))}
+			</span>
+		)
+	},
+	{
+		title: "操作",
+		key: "action",
+		render: (text, record) => (
+			<span>
+				<a href="javascript:;">Delete</a>
+			</span>
+		)
+	}
+];
+
+// const data = [
+// 	{
+// 		key: "1",
+// 		money: "200",
+// 		time: 32,
+// 		tags: ["吃饭"]
+// 	}
+// ];
 
 class AccountBook extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			inputval: "",
+			tabledata: [
+				{
+					key: "1",
+					money: "200",
+					time: "1989-10-26",
+					tags: ["吃饭"]
+				}
+			]
+		};
 	}
-	handleSubmit = () => {};
+	handleChange = e => {
+		let v = e.target.value;
+		this.setState({
+			inputval: v
+		});
+	};
+	handleSubmit = e => {
+		e.preventDefault();
+
+		let time = dayjs().format("YYYY/MM/DD HH:MM");
+		let copytable = this.state.tabledata.slice();
+		copytable.push({
+			key: 'copytable.length + "" + 1',
+			money: this.state.inputval,
+			time: time,
+			tags: ["吃饭"]
+		});
+		this.setState({ tabledata: copytable });
+	};
 	render() {
 		return (
-			<Form onSubmit={this.handleSubmit}>
-				<FormItem label="花费">
-					<Input />
-				</FormItem>
-				<FormItem>
-					<Button type="primary" htmlType="submit">
-						提交
-					</Button>
-				</FormItem>
-			</Form>
+			<div>
+				<Form onSubmit={this.handleSubmit}>
+					<FormItem label="花费">
+						<Input
+							value={this.state.inputval}
+							onChange={this.handleChange}
+						/>
+					</FormItem>
+					<FormItem>
+						<Button type="primary" htmlType="submit">
+							提交
+						</Button>
+					</FormItem>
+				</Form>
+				<Table columns={columns} dataSource={this.state.tabledata} />
+			</div>
 		);
 	}
 }
--- src/cpts/hello.js
+++ src/cpts/hello.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 //import store from "../store/store";
 import { addThings } from "../store/actions";
